2bdc1a248aa3f01daec47d071c099b46fa0ddcf5
Pratik Sawant <sawantklpna@gmail.com>
2026-06-19 16:25:10 +0530

    add the filter def for doc_load

--- configs/services/doc_load.py
+++ configs/services/doc_load.py
@@ -1,33 +1,37 @@
 from langchain_community.document_loaders import GitLoader
 from pydantic import BaseModel
 from pydantic_string_url import HttpUrl
-import os
+from os import path
 import shutil
+from file_filters import codebase_file_filter
 
 class Source(BaseModel):
     path: str
     url: HttpUrl
 
+
 def load_remote_repo(url: str, path: str, branch: str = "main"):
-    if os.path.exists(path):
+    if path.exists(path):
         shutil.rmtree(path)
         
     loader = GitLoader(
-        clone_url=url, 
-        repo_path=path, 
-        branch=branch
+        clone_url = url, 
+        repo_path = path, 
+        branch = branch,
+        file_filter = codebase_file_filter 
     )
     
     docs = loader.load()
     return docs
 
 def load_local_repo(path: str, branch: str = "main"):
-    if not os.path.exists(path):
+    if not path.exists(path):
         raise FileNotFoundError(f"Path {path} does not exist.")
         
     loader = GitLoader(
         repo_path=path, 
-        branch=branch
+        branch=branch,
+        file_filter = codebase_file_filter
     )
     docs = loader.load()
     return docs  
---
+++ configs/services/file_filters.py
@@ -0,0 +1,32 @@
+
+
+def codebase_file_filter(file_path: str) -> bool:
+    allowed_extensions = (
+        ".py", ".js", ".ts", ".jsx", ".tsx",
+        ".go", ".java", ".cpp", ".c", ".h", ".rs", ".rb", ".php",
+        ".md", ".rst", ".txt",
+        ".yaml", ".yml", ".toml", ".json",
+        ".ipynb",
+        ".sql", ".graphql", ".proto", ".html", ".css", ".scss"
+    )
+    
+    excluded_files = (
+        "package-lock.json", "yarn.lock", "poetry.lock", "composer.lock",
+        "tsconfig.tsbuildinfo", "package.json"
+    )
+    
+    excluded_dirs = (
+        "/node_modules/", "/venv/", "/__pycache__/", 
+        "/dist/", "/build/", "/.git/", "/target/"
+    )
+
+    if any(dir_name in file_path for dir_name in excluded_dirs):
+        return False
+        
+    if not file_path.endswith(allowed_extensions):
+        return False
+        
+    if any(file_path.endswith(excluded) for excluded in excluded_files):
+        return False
+        
+    return True
